158bf582b4e3b10a153342711384f81e046d1a5d
Lily Page <neko@nyan.ca>
2021-09-19 19:32:11 +0100

    Update

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var router = express.Router();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var session = require('express-session');
+
+const fs = require('fs');
+let rawdata = fs.readFileSync('dbLogin.json');
+let dbLogin = JSON.parse(rawdata);
+
+const { Pool, Client } = require('pg');
+const pool = new Pool(dbLogin);
+
+var dashRouter = require('./routes/dash.js');
+var loginRouter = require('./routes/login.js');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./public'));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(session({
+  secret: 'puppy sky bison',
+  resave: false,
+  saveUninitialized: true,
+}));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use('/login', loginRouter);
+
+app.use('*', (req, res, next) => {
+  if (req.session.authed) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use('*', dashRouter);
 
 module.exports = app;
